551e97e72f62bdcd4622ee8d0deb5360d7b89af0
AlphaniaMuthee <walphania2@gmail.com>
2020-02-21 16:01:22 +0300

    add hover fuction to js

--- js/scripts.js
+++ js/scripts.js
@@ -11,4 +11,31 @@ $(document).ready(function(){
     $(".product-showing").toggle();
     $(".product-hidden").toggle();
   });
+  $("#contact-form").submit(function() {
+       alert("We have received your message. Thankyou for contacting us!");
+   });
+  $(".black").hover(function(){
+    $(".1").slideToggle();
+  })
+  $(".phoenix").hover(function(){
+    $(".2").slideToggle();
+  })
+  $(".orange").hover(function(){
+    $(".3").slideToggle();
+  })
+  $(".jim-carrey").hover(function(){
+    $(".4").slideToggle();
+  })
+  $(".site").hover(function(){
+    $(".5").slideToggle();
+  })
+  $(".page").hover(function(){
+    $(".6").slideToggle();
+  })
+  $(".burned").hover(function(){
+    $(".7").slideToggle();
+  })
+  $(".giraffe").hover(function(){
+    $(".8").slideToggle();
+  })
 });
